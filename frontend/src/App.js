1c219c7f794b7e9dbc0a0bb2116b1621b55da791
PavithMadushan <pavithmadushan@gmail.com>
2022-11-07 12:32:58 +0530

    home is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      lam dev
+      
+      <Home/>
     </div>
   );
 }
