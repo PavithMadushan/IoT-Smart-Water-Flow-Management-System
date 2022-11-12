ceebcd8f40586e86f7b84f9f7c0072c506f5c602
PavithMadushan <pavithmadushan@gmail.com>
2022-11-13 01:14:38 +0530

    3rd day,Side-bar added

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,11 @@
 import "./sidebar.scss";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import RouterIcon from '@mui/icons-material/Router';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar=()=>{
 
@@ -11,17 +18,39 @@ const Sidebar=()=>{
             <div className="center">
                 <ul>
                     <li>
-                        <span>Dashboard</span>
+                        <DashboardIcon/>
+                        <span>Home</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <ReceiptIcon/>
+                        <span>Billing</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <CalculateIcon/>
+                        <span>Calculator</span>
+                    </li>
+                    <li>
+                        <RouterIcon/>
+                        <span>Devices</span>
+                    </li>
+                    <li>
+                        <SettingsIcon/>
+                        <span>Settings</span>
+                    </li>
+                    <li>
+                        <AccountCircleIcon/>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutIcon/>
+                        <span>Logout</span>
                     </li>
                 </ul>
             </div>
-            <div className="bottom">color options</div>
+            <div className="bottom">
+                <div className="colorOption">color options</div>
+                <div className="colorOption">color options</div>
+                </div>
         </div>
     )
 
